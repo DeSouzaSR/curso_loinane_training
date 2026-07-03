18a72175868c8b83e531b01d200bff95418b5aac
DeSouzaSR <sandro.fisica@gmail.com>
2026-07-03 16:29:02 -0300

    Retornei ao curso

---
+++ curso-loiane-java-basico/src/curso/loiane/java/basico/CursoLoianeJavaBasico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package curso.loiane.java.basico;
+
+/**
+ *
+ * @author srsouza
+ */
+public class CursoLoianeJavaBasico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
